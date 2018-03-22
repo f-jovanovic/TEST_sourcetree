1a448900362ff0e21a6542c5901eb658acaf854b
Filip Jovanović <filip.jovanovic14@gmail.com>
2018-03-22 22:25:27 +0100

    Add "Show all" option
    ADDED
    - case 2 with code for writting out each person object and all its entered properties
    
    CHANGED
    - list initialization was placed outside of the do-while loop, thus fixing problems with data being overwritten

--- GIT_test/GIT_test/Program.cs
+++ GIT_test/GIT_test/Program.cs
@@ -11,18 +11,18 @@ namespace GIT_test
         static void Main(string[] args)
         {
             string choice = "";
+            List<Person> people = new List<Person>();
+            Person newPerson = null;
+
             do
             {
                 Console.Clear();
 
-                Console.WriteLine("WELCOME\n1. Name entry \n2. Phone number entry\n3. Show all\n4. Search by name \nx - EXIT");
+                Console.WriteLine("WELCOME\n1. Data entry \n2. Show all\n3. Search by name \nx - EXIT");
                 Console.Write(": ");
                 choice = Console.ReadLine();
 
                 Console.Clear();
-
-                List<Person> people = new List<Person>();
-                Person newPerson = null;
 
                 switch (choice)
                 {
@@ -32,7 +32,7 @@ namespace GIT_test
                         newPerson.Name = Console.ReadLine();
                         Console.Clear();
                         string tmpChoice = "y";
-                        while (tmpChoice=="y")
+                        while (tmpChoice == "y")
                         {
                             Console.WriteLine("Enter phone number: ");
                             newPerson.numbers.Add(Console.ReadLine());
@@ -40,10 +40,30 @@ namespace GIT_test
                             Console.Write(": ");
                             tmpChoice = Console.ReadLine();
                         }
+                        
                         people.Add(newPerson);
-                        Console.WriteLine("<<Continue>>");
+                        Console.WriteLine("\n\n<<Continue>>");
                         Console.ReadKey();
                         break;
+
+                    case "2":
+                        foreach (var item in people)
+                        {
+                            
+                            Console.WriteLine("Name: " + item.Name);
+                            int tmp = 0;
+                            foreach (var help in item.numbers)
+                            {
+                                tmp++;
+                                Console.WriteLine("Phone number {0}: " + help, tmp);
+                            }
+                            Console.WriteLine();
+                        }
+
+                        Console.WriteLine("\n\n<<Continue>>");
+                        Console.ReadKey();
+                        break;
+
                     default:
                         break;
                 }
